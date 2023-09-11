742ad8f4efb5143cf0d30f5851a04f8b8388bb9a
Caio Tertuliano Ribeiro <caio.tertuliano@indicium.tech>
2023-09-11 18:49:49 +0000

    Modificações na dim_produtos

--- models/marts/dim_produtos.sql
+++ models/marts/dim_produtos.sql
@@ -38,6 +38,7 @@ WITH
             , detalhe_pedidos.codigo_rastreamento_frete
             , detalhe_pedidos.qtde_pedido
             , detalhe_pedidos.preco_unitario
+            , detalhe_pedidos.desconto_preco_unitario
         FROM produtos
         LEFT JOIN detalhe_pedidos
         ON produtos.id_produto = detalhe_pedidos.id_produto
@@ -46,33 +47,7 @@ WITH
     , transformacoes as (
         SELECT
             row_number() OVER (ORDER BY id_produto) as sk_produto
-            , id_produto
-            , id_subcategoria
-            , id_pedido
-            , id_detalhe_pedido
-            , id_oferta
-            , nome_produto
-            , sku_produto
-            , flag_produto_acabado
-            , cor_produto
-            , nivel_estoque_seguranca_minimo
-            , ponto_de_pedido
-            , custo_padrao
-            , preco_venda_sugerido
-            , tamanho_produto
-            , unidade_medida_tamanho_produto
-            , peso_produto
-            , unidade_medida_peso_produto
-            , dias_para_producao_produto
-            , linha_produto
-            , classe_produto
-            , estilo_produto
-            , data_inicio_venda
-            , data_fim_venda
-            , data_descontinuacao_produto
-            , codigo_rastreamento_frete
-            , qtde_pedido
-            , preco_unitario
+            , *
         FROM join_tabelas
     )
 
