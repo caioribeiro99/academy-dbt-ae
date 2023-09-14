3b16f4f980ec1b220a61a7d5cfe8905550fffd6b
Caio Tertuliano Ribeiro <caio.tertuliano@indicium.tech>
2023-09-14 18:02:50 +0000

    Correção na dim_motivo_vendas para adicionar um registro genérico que servirá para substituir valor nulos de chave na tabela fato final

--- models/marts/dim_motivo_venda.sql
+++ models/marts/dim_motivo_venda.sql
@@ -11,6 +11,18 @@ WITH
         FROM motivo_venda
     )
 
+    , adicionar_motivo_venda_generico as (
+        SELECT
+            99 as sk_motivo_venda
+            , 999 as id_motivo_venda
+            , 'Não especificado' as motivo_venda
+            , 'Não especificado' as categoria_motivo_venda
+            , cast('2008-04-30' as timestamp) as data_ultima_atualizacao
+    )
+
 SELECT *
 FROM transformacoes
+UNION ALL
+SELECT *
+FROM adicionar_motivo_venda_generico
 ORDER BY sk_motivo_venda
